5d0505a9976cded450a73ed7b18553d8a3b34348
joecsss <joecs002@gmail.com>
2025-09-20 20:55:10 +0700

    login/register

---
+++ src/main/java/com/example/doctoralia/dto/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.example.doctoralia.dto;
+
+public class MessageResponse {
+    private String message;
+
+   public MessageResponse() {}
+
+    public MessageResponse(String message) {
+       this.message = message;
+    }
+
+    public String getMessage() {
+       return message;
+    }
+
+    public void setMessage(String message) {}
+}
